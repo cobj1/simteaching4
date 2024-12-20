7b586a5b0abfd2d15d21b85fd0839316371d9043
WANG <541980655@qq.com>
2024-12-20 15:47:12 +0800

    用户菜单增加头像和用户名

--- src/components/user/UserPanel.vue
+++ src/components/user/UserPanel.vue
@@ -2,13 +2,23 @@
   <div class="mr-2">
     <v-btn v-if="accountStore.token" class="ms-1" icon>
       <v-avatar image="@/assets/avatar/default-avatar.svg" />
-      <v-menu activator="parent">
-        <v-list density="compact" nav>
-          <v-list-item append-icon="mdi-home" link title="首页" @click="router.replace('/')" />
-          <v-list-item append-icon="mdi-school" link title="控制台" to="/console/home" />
-          <v-list-item append-icon="mdi-account-cog" link title="账号设置" @click="dialog = true" />
-          <v-list-item append-icon="mdi-logout" link title="注销" @click="accountStore.logout" />
-        </v-list>
+      <v-menu activator="parent" width="280">
+        <v-card variant="text" class=" rounded-lg	 border">
+          <v-responsive class="bg-surface-light rounded-0" style="height: 88px;"></v-responsive>
+          <div class="text-center mt-n9 mb-4">
+            <v-avatar size="72" class="border-md border-opacity-100 border-surface-light bg-surface">
+              <v-img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"></v-img>
+            </v-avatar>
+            <div class="text-h6 mt-2">{{ accountStore.info.name }}</div>
+          </div>
+          <VDivider></VDivider>
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-home" link title="首页" @click="router.replace('/')" />
+            <v-list-item prepend-icon="mdi-school" link title="控制台" to="/console/home" />
+            <v-list-item prepend-icon="mdi-account-cog" link title="账号设置" @click="dialog = true" />
+            <v-list-item prepend-icon="mdi-logout" link title="注销" @click="accountStore.logout" />
+          </v-list>
+        </v-card>
       </v-menu>
     </v-btn>
     <v-btn v-else class="text-none me-2" prepend-icon="mdi-login" @click="goLogin">
